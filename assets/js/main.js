276adacc02aabee18cdc6548e9078e908bafb794
240240 <89885166+240240@users.noreply.github.com>
2021-09-13 10:17:38 +0900

    問題の内容変えた

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'とらぶる',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'うらない',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'ちょう',
           // q2: 'かかか',
           // q3: 'ききき',
         }
